4e9c2012aa841f293d0397aa6e4a3b431a54dd83
tyler9937 <tyler9937@users.noreply.github.com>
2020-06-05 12:07:01 -0400

    Finished Sprint

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,36 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    '''
+    Takes in a list of weights, the length of that list, and a limit number
+    '''
+    cache = {}
 
-    return None
+    # checking if value in cache
+    if limit in cache:
+        return cache[limit]
+
+    else:
+        # Calculate the entry to the cache
+        entry = None
+
+        for i1, w1 in enumerate(weights):
+            w2 = limit - w1
+
+            if w2 in weights:
+                if w2 != w1:
+                    i2 = weights.index(w2)
+                else:
+                    weights[i1] = w1 + 1
+                    i2 = weights.index(w2)
+
+                if i1 > i2:
+                    entry = (i1, i2)
+                    break
+                else:
+                    entry = (i2, i1)
+                    break
+
+        cache[limit] = entry
+
+    return entry
+
+print(get_indices_of_item_weights([4, 4], 2, 8))
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,10 +1,24 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    '''
+    takes in nested list of numbers
+    '''
+    dict_list = []
+    main_dict = {}
 
-    return result
+    # Turning each array into dict
+    for index, array in enumerate(arrays):
+        dict1 = {}
+        for num in array:
+            dict1.update({num: None})
+
+        # Adding newly created dict to main dict
+        if len(main_dict) == 0:
+            main_dict = dict1
+        else:
+            main_dict = {x:dict1[x] for x in dict1
+                            if x in main_dict}
+
+    return list(main_dict)
 
 
 if __name__ == "__main__":
@@ -13,5 +27,6 @@ if __name__ == "__main__":
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
 
     print(intersection(arrays))
